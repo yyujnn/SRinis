5d50d2b15cd404567f04a52a13a23efd6b337129
devpark435 <devpark435@gmail.com>
2024-02-28 17:48:31 +0900

    feat #4 - uiview outlet & cornerRadius

--- SRinis/ListCollectionViewCell.swift
+++ SRinis/ListCollectionViewCell.swift
@@ -10,9 +10,12 @@ class ListCollectionViewCell:UICollectionViewCell{
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var uiview: UIView!
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.cornerRadius = 15
         self.layer.masksToBounds = true
+        self.uiview.layer.cornerRadius = 35
+        self.uiview.layer.masksToBounds = true
     }
 }
