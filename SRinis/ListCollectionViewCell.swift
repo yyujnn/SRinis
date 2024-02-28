a37ae03518170d5efc440f06bfc3c9f46a7fd3df
devpark435 <devpark435@gmail.com>
2024-02-28 16:28:48 +0900

    test

---
+++ SRinis/ListCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  ListCollectionViewCell.swift
+//  SRinis
+//
+//  Created by 박현렬 on 2/28/24.
+//
+
+import Foundation
---
+++ SRinis/Protocol.swift
@@ -0,0 +1,19 @@
+//
+//  Protocol.swift
+//  MiniProject
+//
+//  Created by 정유진 on 2024/02/28.
+//
+
+import Foundation
+
+protocol Member {
+    var name: String { get }
+    var email: String { get }
+    var mbti: String { get }
+    var hobby: String { get }
+    var shortDescription: String { get } // 소개글
+    var isLeader: Bool { get } // 팀장 여부
+    var blogURL: String { get } // 블로그 주소
+    var githubURL: String { get } // 깃허브 주소
+}
